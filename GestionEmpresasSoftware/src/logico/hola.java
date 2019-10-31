9ea7c27cd0e808676b9647c141518672aabdad6c
admin <admin@DESKTOP-6C4SHFB>
2019-10-31 15:36:33 -0400

    sets and gets

--- GestionEmpresasSoftware/src/logico/hola.java
+++ GestionEmpresasSoftware/src/logico/hola.java
@@ -4,4 +4,17 @@ public class hola {
 	
 	private String hola;
 
+	public hola(String hola) {
+		super();
+		this.hola = hola;
+	}
+
+	public String getHola() {
+		return hola;
+	}
+
+	public void setHola(String hola) {
+		this.hola = hola;
+	}
+
 }
