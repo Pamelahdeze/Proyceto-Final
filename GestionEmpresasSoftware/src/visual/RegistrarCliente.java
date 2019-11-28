6e42148b1ea7acd46944fb3a9d5a8826cffe749c
Principal <Principal@Hp-Laptop>
2019-11-28 16:32:25 -0400

    Arreglo de diseño RegistrarCliente

--- GestionEmpresasSoftware/src/visual/RegistrarCliente.java
+++ GestionEmpresasSoftware/src/visual/RegistrarCliente.java
@@ -21,6 +21,17 @@ import javax.swing.JSpinner;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.SwingConstants;
+import java.awt.Cursor;
+import javax.swing.ImageIcon;
+import java.awt.SystemColor;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
 
 public class RegistrarCliente extends JDialog {
 
@@ -48,90 +59,145 @@ public class RegistrarCliente extends JDialog {
 	 */
 	public RegistrarCliente() {
 		setTitle("Registrar Cliente");
-		setBounds(100, 100, 264, 230);
+		setBounds(100, 100, 470, 256);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setForeground(new Color(175, 238, 238));
+		contentPanel.setBackground(SystemColor.activeCaption);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
-		JLabel lblID = new JLabel("ID:");
-		lblID.setBounds(10, 20, 46, 14);
-		contentPanel.add(lblID);
+		setUndecorated(true);
 		
 		try {
 			textId = new JFormattedTextField(new MaskFormatter("##-###"));
+			textId.setFont(new Font("Tahoma", Font.PLAIN, 13));
+			textId.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.activeCaptionText));
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		textId.setBounds(77, 17, 100, 20);
+		textId.setBounds(211, 35, 100, 20);
 		contentPanel.add(textId);
 		textId.setColumns(10);
 		
-		JLabel lblNombre = new JLabel("Nombre:");
-		lblNombre.setBounds(10, 54, 64, 14);
-		contentPanel.add(lblNombre);
-		
 		textNombre = new JTextField();
+		textNombre.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textNombre.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.desktop));
 		textNombre.setColumns(10);
-		textNombre.setBounds(76, 51, 162, 20);
+		textNombre.setBounds(211, 90, 230, 20);
 		contentPanel.add(textNombre);
 		
-		JLabel lblDireccin = new JLabel("Direcci\u00F3n:");
-		lblDireccin.setBounds(10, 88, 64, 14);
-		contentPanel.add(lblDireccin);
-		
 		textDireccion = new JTextField();
+		textDireccion.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textDireccion.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.desktop));
 		textDireccion.setColumns(10);
-		textDireccion.setBounds(76, 85, 162, 20);
+		textDireccion.setBounds(211, 145, 230, 20);
 		contentPanel.add(textDireccion);
 		
-		JLabel lblCantidadProyectos = new JLabel("Cantidad Proyectos:");
-		lblCantidadProyectos.setBounds(10, 122, 138, 14);
-		contentPanel.add(lblCantidadProyectos);
-		
 		JSpinner spinnerCantidad = new JSpinner();
-		spinnerCantidad.setBounds(137, 119, 40, 20);
+		spinnerCantidad.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		spinnerCantidad.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.desktop));
+		spinnerCantidad.setBounds(211, 200, 100, 20);
 		contentPanel.add(spinnerCantidad);
+		
+		JLabel close = new JLabel("X");
+		close.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		close.setHorizontalAlignment(SwingConstants.CENTER);
+		close.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				dispose();
+			}
+		});
+		close.setForeground(Color.WHITE);
+		close.setFont(new Font("Tahoma", Font.BOLD, 15));
+		close.setBounds(424, 11, 46, 14);
+		contentPanel.add(close);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.controlHighlight);
+		panel.setBounds(0, 0, 178, 256);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setForeground(Color.DARK_GRAY);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setIcon(new ImageIcon(RegistrarCliente.class.getResource("/imagenes/owner.png")));
+		lblNewLabel.setBounds(10, 38, 158, 166);
+		panel.add(lblNewLabel);
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("Registrar");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						if(validarCamposCliente())
-						{	
-							Cliente cliente = new Cliente(textId.getText(),textNombre.getText(),textDireccion.getText());
-							Empresa.getInstance().RegistrarCliente(cliente);
-							JOptionPane.showMessageDialog(miDialog, "Cliente registrado exitosamente", "Información", JOptionPane.INFORMATION_MESSAGE);
-						    clear();
-						}
-					}
-
-					private void clear() {
-						textId.setText(null);
-						textNombre.setText(null);
-						spinnerCantidad.setValue(0);
-						textDireccion.setText(null);
-					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancelar");
-				cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						dispose();
-					}
-				});
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
+			JButton cancelButton = new JButton("Cancelar");
+			cancelButton.setBackground(SystemColor.textHighlightText);
+			cancelButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			cancelButton.setHorizontalTextPosition(SwingConstants.CENTER);
+			cancelButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+			cancelButton.setBorder(new LineBorder(Color.BLACK, 1, true));
+			cancelButton.setBounds(93, 222, 75, 23);
+			panel.add(cancelButton);
+			cancelButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					dispose();
+				}
+			});
+			cancelButton.setActionCommand("Cancel");
 		}
+		{
+			JButton okButton = new JButton("Registrar");
+			okButton.setBackground(SystemColor.textHighlightText);
+			okButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+			okButton.setHorizontalTextPosition(SwingConstants.CENTER);
+			okButton.setFont(new Font("Tahoma", Font.BOLD, 13));
+			okButton.setOpaque(false);
+			okButton.setBorder(new LineBorder(Color.BLACK, 1, true));
+			okButton.setBounds(10, 222, 77, 23);
+			panel.add(okButton);
+			okButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if(validarCamposCliente())
+					{	
+						Cliente cliente = new Cliente(textId.getText(),textNombre.getText(),textDireccion.getText());
+						Empresa.getInstance().RegistrarCliente(cliente);
+						JOptionPane.showMessageDialog(miDialog, "Cliente registrado exitosamente", "Información", JOptionPane.INFORMATION_MESSAGE);
+					    clear();
+					}
+				}
+
+				private void clear() {
+					textId.setText(null);
+					textNombre.setText(null);
+					spinnerCantidad.setValue(0);
+					textDireccion.setText(null);
+				}
+			});
+			okButton.setActionCommand("OK");
+			getRootPane().setDefaultButton(okButton);
+		}
+		
+		JLabel lblID = new JLabel("ID:");
+		lblID.setBounds(211, 12, 46, 14);
+		contentPanel.add(lblID);
+		lblID.setForeground(SystemColor.desktop);
+		lblID.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+		
+		JLabel lblCantidadProyectos = new JLabel("Cantidad Proyectos:");
+		lblCantidadProyectos.setBounds(211, 176, 138, 14);
+		contentPanel.add(lblCantidadProyectos);
+		lblCantidadProyectos.setForeground(SystemColor.desktop);
+		lblCantidadProyectos.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(211, 66, 64, 14);
+		contentPanel.add(lblNombre);
+		lblNombre.setForeground(SystemColor.desktop);
+		lblNombre.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+		
+		JLabel lblDireccin = new JLabel("Direcci\u00F3n:");
+		lblDireccin.setBounds(211, 121, 64, 14);
+		contentPanel.add(lblDireccin);
+		lblDireccin.setForeground(SystemColor.desktop);
+		lblDireccin.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
 	}
 	
 	private boolean validarCamposCliente() {
