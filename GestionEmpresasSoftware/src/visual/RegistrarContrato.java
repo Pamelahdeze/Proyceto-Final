d623630e01d69e8bd2d76dca7a2dd29d98f41809
Isara Rosmel <Isara Rosmel@Toshiba>
2019-12-04 22:07:23 -0400

    ventana de contrato más decente

--- GestionEmpresasSoftware/src/visual/RegistrarContrato.java
+++ GestionEmpresasSoftware/src/visual/RegistrarContrato.java
@@ -1,23 +1,40 @@
 package visual;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+
+import logico.Cliente;
+import logico.Contrato;
+import logico.Empresa;
+
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
 import javax.swing.JFormattedTextField;
+import java.awt.SystemColor;
+import java.awt.Font;
+import javax.swing.ImageIcon;
 
 public class RegistrarContrato extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_3;
+	private JTextField txtIdContrato;
+	private JTextField txtIdCliente;
 
 	/**
 	 * Launch the application.
@@ -37,75 +54,144 @@ public class RegistrarContrato extends JDialog {
 	 */
 	public RegistrarContrato() {
 		setTitle("Registrar Contrato");
-		setBounds(100, 100, 300, 192);
+		setBounds(100, 100, 571, 343);
 		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBackground(SystemColor.activeCaption);
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		setUndecorated(true);
+		
 		{
 			JLabel lblIDContrato = new JLabel("ID Contrato:");
-			lblIDContrato.setBounds(10, 11, 75, 14);
+			lblIDContrato.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+			lblIDContrato.setBounds(21, 68, 116, 18);
 			contentPanel.add(lblIDContrato);
 		}
 		{
-			textField = new JTextField();
-			textField.setBounds(92, 8, 182, 20);
-			contentPanel.add(textField);
-			textField.setColumns(10);
+			txtIdContrato = new JTextField();
+			txtIdContrato.setBounds(159, 66, 182, 20);
+			contentPanel.add(txtIdContrato);
+			txtIdContrato.setColumns(10);
+			txtIdContrato.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.activeCaptionText));
 		}
 		{
 			JLabel lblFechaInicial = new JLabel("Fecha Inicial:");
-			lblFechaInicial.setBounds(10, 36, 75, 14);
+			lblFechaInicial.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+			lblFechaInicial.setBounds(21, 122, 116, 18);
 			contentPanel.add(lblFechaInicial);
 		}
 		{
 			JLabel lblFechaEntrega = new JLabel("Fecha Entrega:");
-			lblFechaEntrega.setBounds(10, 61, 75, 14);
+			lblFechaEntrega.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+			lblFechaEntrega.setBounds(21, 176, 116, 18);
 			contentPanel.add(lblFechaEntrega);
 		}
 		{
 			JLabel lblIdCliente = new JLabel("ID Cliente:");
-			lblIdCliente.setBounds(10, 89, 75, 14);
+			lblIdCliente.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 13));
+			lblIdCliente.setBounds(21, 227, 116, 18);
 			contentPanel.add(lblIdCliente);
 		}
 		{
-			textField_3 = new JTextField();
-			textField_3.setColumns(10);
-			textField_3.setBounds(92, 86, 182, 20);
-			contentPanel.add(textField_3);
+			txtIdCliente = new JTextField();
+			txtIdCliente.setColumns(10);
+			txtIdCliente.setBounds(159, 227, 182, 20);
+			contentPanel.add(txtIdCliente);
+			txtIdCliente.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.activeCaptionText));
 		}
 		
-		JFormattedTextField formattedTextField = new JFormattedTextField();
-		formattedTextField.setBounds(92, 33, 86, 20);
-		contentPanel.add(formattedTextField);
+		JFormattedTextField ftxtFechaInicial = new JFormattedTextField();
+		ftxtFechaInicial.setBounds(159, 120, 182, 20);
+		contentPanel.add(ftxtFechaInicial);
+		ftxtFechaInicial.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.activeCaptionText));
 		
-		JFormattedTextField formattedTextField_1 = new JFormattedTextField();
-		formattedTextField_1.setBounds(92, 61, 86, 20);
-		contentPanel.add(formattedTextField_1);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("Registrar");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
+		JFormattedTextField ftxtFechaEntrega = new JFormattedTextField();
+		ftxtFechaEntrega.setBounds(159, 174, 182, 20);
+		contentPanel.add(ftxtFechaEntrega);
+		ftxtFechaEntrega.setBorder(new MatteBorder(0, 0, 2, 0, (Color) SystemColor.activeCaptionText));
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.controlHighlight);
+		panel.setBounds(359, 0, 212, 343);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JButton button = new JButton("Registrar");
+		//button.addActionListener(new ActionListener() {
+		/*public void actionPerformed(ActionEvent e) {
+				if(validarCamposContrato())
+				{
+					if(contrato==null) {
+						Contrato contrac = new Contrato(txtIdContrato.getText(), proyecto, cliente, fechaInicio, fechaEntrega, fechaProrroga)
+						Empresa.getInstance().RegistrarCliente(cliente);
+						JOptionPane.showMessageDialog(miDialog, "Cliente registrado exitosamente", "Información", JOptionPane.INFORMATION_MESSAGE);
+					    clear();
 					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancelar");
-				cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent e) {
-						dispose();
+					else {
+						cliente.setNombre(textNombre.getText());
+						cliente.setDireccion(textDireccion.getText());
+						cliente.setIndentificador(textId.getText());
+						cliente.setCantMaxProyectos((int)spinnerCantidad.getValue());
+						clear();
+						Empresa.getInstance().escribirDatos();
+						cliente = null;
+						JOptionPane.showMessageDialog(miDialog, "Cliente actualizado exitosamente", "Información", JOptionPane.INFORMATION_MESSAGE);
+						closeDialog();
+						
 					}
-				});
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				}
+				
+			}*/
+		//});
+		button.setFont(new Font("Tahoma", Font.BOLD, 13));
+		button.setActionCommand("OK");
+		button.setBounds(37, 307, 146, 25);
+		panel.add(button);
+		button.setBorder(new LineBorder(new Color(0, 0, 0), 3, true));
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(RegistrarContrato.class.getResource("/imagenes/policy.png")));
+		lblNewLabel.setBounds(30, 24, 192, 272);
+		panel.add(lblNewLabel);
+		
+		JLabel close = new JLabel("X");
+		close.setBounds(156, 11, 46, 14);
+		panel.add(close);
+		close.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		close.setHorizontalAlignment(SwingConstants.CENTER);
+		close.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				dispose();
 			}
-		}
+		});
+		close.setForeground(SystemColor.desktop);
+		close.setFont(new Font("Tahoma", Font.BOLD, 15));
+		
+		
+		/*private boolean validarCamposContrato() {
+			String mensajeValidacion = "";
+			boolean datosValidos = true;
+			
+			if(txtIdContrato.getText().trim().isEmpty()) {
+				mensajeValidacion += "*El identificador es necesario\n";
+				datosValidos = false;
+			}
+			if(ftxtFechaInicial.getText().trim().isEmpty()) {
+				mensajeValidacion += "*El campo fecha inicial es necesario\n";
+				datosValidos = false;
+			}
+			if(ftxtFechaEntrega.getText().trim().isEmpty()) {
+				mensajeValidacion += "*El campo fecha inicial es necesario\n";
+				datosValidos = false;
+			}
+			if(txtIdCliente.getText().trim().isEmpty()) {
+				mensajeValidacion += "*El identificador de cliente es necesario\n";
+				datosValidos = false;
+			}
+			
+			return datosValidos;
+		}*/
 	}
 }
